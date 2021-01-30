6b33994829b87815c007dd20b83bd44b03c57f5e
Marcelo Souza <dn@dn32.com.br>
2021-01-30 20:27:25 -0300

    Projeto adicionado 2

---
+++ SQS.Consumidor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Consumidor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+            var request = new ReceiveMessageRequest
+            {
+                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/355552168393/teste"
+            };
+
+            while (true)
+            {
+                var response = await client.ReceiveMessageAsync(request);
+
+                foreach (var mensagem in response.Messages)
+                {
+                    Console.WriteLine(mensagem.Body);
+                    await client.DeleteMessageAsync("https://sqs.sa-east-1.amazonaws.com/355552168393/teste", mensagem.ReceiptHandle);
+                }
+            }
+        }
+    }
+}
---
+++ SQS.Fornecedor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Fornecedor
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+            var request = new SendMessageRequest
+            {
+                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/355552168393/teste",
+                MessageBody = "teste 123"
+            };
+
+            await client.SendMessageAsync(request);
+        }
+    }
+}
