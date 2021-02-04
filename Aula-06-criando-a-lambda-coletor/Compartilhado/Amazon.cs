5c4b126184ed373762dd39ced501196bcf66fc06
Marcelo Souza <dn@dn32.com.br>
2021-02-04 09:02:10 -0300

    Aula 06 Adicionada

---
+++ Aula-06-criando-a-lambda-coletor/Compartilhado/Amazon.cs
@@ -0,0 +1,30 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.Model;
+using Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Compartilhado
+{
+    public static class Amazon
+    {
+        public static async Task SalvarAsync(this Pedido pedido)
+        {
+            var client = new AmazonDynamoDBClient(RegionEndpoint.SAEast1);
+            var context = new DynamoDBContext(client);
+            await context.SaveAsync(pedido);
+        }
+
+        public static T ToObject<T>(this Dictionary<string, AttributeValue> dictionary)
+        {
+            var client = new AmazonDynamoDBClient(RegionEndpoint.SAEast1);
+            var context = new DynamoDBContext(client);
+
+            var doc = Document.FromAttributeMap(dictionary);
+            return context.FromDocument<T>(doc);
+        }
+    }
+}
---
+++ Aula-06-criando-a-lambda-coletor/Compartilhado/Model/Pedido.cs
@@ -0,0 +1,35 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public enum StatusDoPedido
+    {
+        Coletado,
+        Pago,
+        Faturado
+    }
+
+    [DynamoDBTable("pedidos")]
+    public class Pedido
+    {
+        public string Id { get; set; }
+
+        public decimal ValorTotal { get; set; }
+
+        public DateTime DataDeCriacao { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+
+        public Cliente Cliente { get; set; }
+
+        public Pagamento Pagamento { get; set; }
+
+        public string JustificativaDeCancelamento { get; set; }
+
+        public StatusDoPedido Status { get; set; }
+
+        public bool Cancelado { get; set; }
+    }
+}
